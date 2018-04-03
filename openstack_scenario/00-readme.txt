42a39cfad78ee37ecee3443cc1d5d5bf0f6af39d
Carlos Vega <c.vegag5@gmail.com>
2018-04-03 11:55:49 +0200

    Add base OpenStack scenario

---
+++ openstack_scenario/00-readme.txt
@@ -0,0 +1,7 @@
+
+
+- Para crear el escenario empaquetado:
+
+    bin/pack-scenario-with-rootfs # con rootfs
+
+    bin/pack-scenario             # sin rootfs
---
+++ openstack_scenario/start.sh
@@ -0,0 +1,8 @@
+apt-get install -y sshpass
+vnx -f openstack_gredos.xml -v -t
+vnx_config_nat ExtNet eno1
+sleep 40
+vnx -f openstack_gredos.xml -x start-all,load-img
+sshpass -p 'xxxx' scp -o StrictHostKeyChecking=no conf/controller/glance/Xenial-GREDOS.qcow2 root@controller:/tmp/images
+vnx -f openstack_gredos.xml -x create-img-local,step9
+vnx -f openstack_gredos.xml -x create-tacker
