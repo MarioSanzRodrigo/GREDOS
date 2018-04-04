953ec6e80652958550f242e316311cd8d0991428
Carlos Vega <c.vegag5@gmail.com>
2018-04-04 11:05:09 +0200

    Fix pox scenario

--- clients_scenario/scripts/bananapi/controllers.sh
+++ clients_scenario/scripts/bananapi/controllers.sh
@@ -5,20 +5,20 @@ while true
 do
 ping -c 3 $controller_ip > /dev/null
   if [ $? -eq 0 ]; then
-    echo conecta
+    #echo conecta
     /usr/local/bin/ovs-vsctl show | grep tcp:$controller_ip > /dev/null
     if [ $? -eq 1 ];then 
       nc -zv $controller_ip 6633
       if [ $? -eq 0 ];then 
-        echo Conecta a pox
+        #echo Conecta a pox
         /usr/local/bin/ovs-vsctl set-controller br0 tcp:$controller_ip:6633
       else
-        echo Conecta a Fl
+        #echo Conecta a Fl
         /usr/local/bin/ovs-vsctl set-controller br0 tcp:$controller_ip:6653
       fi
     fi
   else
-    echo 'no conecta'
+    #echo 'no conecta'
     /usr/local/bin/ovs-vsctl show | grep tcp:$br0_ip:6633 > /dev/null
     if [ $? -eq 1 ];then 
       #Conecta a pox local
