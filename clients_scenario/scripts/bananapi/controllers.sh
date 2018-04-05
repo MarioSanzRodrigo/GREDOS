26397b8639e1d86431c334750a4d61faebfcb509
Carlos Vega <c.vegag5@gmail.com>
2018-04-05 12:27:46 +0200

    Add Floodlight scenarios

--- clients_scenario/scripts/bananapi/controllers.sh
+++ clients_scenario/scripts/bananapi/controllers.sh
@@ -4,7 +4,7 @@ br0_ip=$2
 TVlanID=$3
 while true
 do
-ping -c 3 $controller_ip > /dev/null
+ping -c 3 $controller_ip  > /dev/null
   if [ $? -eq 0 ]; then
     #echo conecta
     /usr/local/bin/ovs-vsctl show | grep tcp:$controller_ip > /dev/null
@@ -12,10 +12,13 @@ ping -c 3 $controller_ip > /dev/null
       nc -zv $controller_ip 6633
       if [ $? -eq 0 ];then 
         #echo Conecta a pox
+        vconfig add eth1 $TVlanID
+        ifconfig eth1.$TVlanID up
         /usr/local/bin/ovs-vsctl set-controller br0 tcp:$controller_ip:6633
         /usr/local/bin/ovs-vsctl add-port br0 eth1.$TVlanID
       else
         #echo Conecta a Fl
+        vconfig rem eth1.$TVlanID
         /usr/local/bin/ovs-vsctl set-controller br0 tcp:$controller_ip:6653
         /usr/local/bin/ovs-vsctl add-del br0 eth1.$TVlanID
       fi
