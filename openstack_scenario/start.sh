28365281eae780eeccf84c5d39c23b14b5a95be5
Carlos Vega <c.vegag5@gmail.com>
2018-04-13 10:57:00 +0200

    Change start script

--- openstack_scenario/start.sh
+++ openstack_scenario/start.sh
@@ -4,5 +4,6 @@ vnx_config_nat ExtNet eno1
 sleep 40
 vnx -f openstack_gredos.xml -x start-all,load-img
 sshpass -p 'xxxx' scp -o StrictHostKeyChecking=no conf/controller/glance/Xenial-GREDOS.qcow2 root@controller:/tmp/images
-vnx -f openstack_gredos.xml -x create-img-local,step9
+vnx -f openstack_gredos.xml -x create-img-local
+vnx -f openstack_gredos.xml -x step9
 vnx -f openstack_gredos.xml -x create-tacker
