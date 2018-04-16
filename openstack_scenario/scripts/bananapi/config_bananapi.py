4fa6bf85971c4961ede5423472b25fcee1f133fc
Carlos Vega <c.vegag5@gmail.com>
2018-04-16 13:56:52 +0200

    Create scripts to ip addressing plan

---
+++ openstack_scenario/scripts/bananapi/config_bananapi.py
@@ -0,0 +1,58 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys, os, json
+from time import sleep
+
+config_file_path = '/etc/base.rc.local'
+out_file_path = '/etc/rc.local'
+
+
+def lauch():
+
+        while True:
+
+                os.system('clear')
+                print '\n---Residential Gateway Configuration File Creator:'
+                print '\n\tOptions:'
+                print '\t1: Create a Configuration File'
+                print '\t2: Exit'
+
+                option = int(raw_input('\n\t*Select an Option:'))
+
+                if option == 1:
+                        os.system('clear')
+                        TVlanID = raw_input('\n\t*Introduce the traffic VLAN ID')
+                        OFVlanID = raw_input('\n\t*Introduce the OpenFlow VLAN ID:')
+                        trafficSubnet = "10.1."+TVLanID+".0/24"
+                        OFSubnet = "10.2."+OFLanID+".0/24"
+
+                        # Read in the file
+                        with open(config_file_path, 'r') as file :
+                          filedata = file.read()
+
+                        # Replace the target string
+                        filedata = filedata.replace('controllerIP', OFSubnet.split("0/24")[0]+"30")
+                        filedata = filedata.replace('OFVlanID', OFVlanID)
+                        filedata = filedata.replace('OFIP', OFSubnet.split("0/24")[0]+"87")
+                        filedata = filedata.replace('gwIP', trafficSubnet.split("0/24")[0]+"254")
+                        filedata = filedata.replace('br0IP', trafficSubnet.split("0/24")[0]+"1" )
+                        filedata = filedata.replace('TVlanID', TVlanID)
+
+                        # Write the file out again
+                        with open(out_file_path, 'w') as file:
+                          file.write(filedata)
+                        os.chmod(out_file_path, 0777)
+
+                elif option == 2:
+                        os.system('clear')
+                        sys.exit()
+
+                else:
+                        os.system('clear')
+                        print '\n\t!!!!!!!!!! Invalid option, try again !!!!!!!!!!'
+                        sleep(1)
+#---------------------------------------------------------------------------------------------------------------
+
+lauch()
+
---
+++ openstack_scenario/scripts/bananapi/controllers.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+controller_ip=$1
+br0_ip=$2
+TVlanID=$3
+while true
+do
+ping -c 3 $controller_ip  > /dev/null
+  if [ $? -eq 0 ]; then
+    #echo conecta
+    /usr/local/bin/ovs-vsctl show | grep tcp:$controller_ip > /dev/null
+    if [ $? -eq 1 ];then 
+      nc -zv $controller_ip 6633
+      if [ $? -eq 0 ];then 
+        #echo Conecta a pox
+        vconfig add eth1 $TVlanID
+        ifconfig eth1.$TVlanID up
+        /usr/local/bin/ovs-vsctl set-controller br0 tcp:$controller_ip:6633
+        /usr/local/bin/ovs-vsctl add-port br0 eth1.$TVlanID
+      else
+        #echo Conecta a Fl
+        vconfig rem eth1.$TVlanID
+        /usr/local/bin/ovs-vsctl set-controller br0 tcp:$controller_ip:6653
+        /usr/local/bin/ovs-vsctl add-del br0 eth1.$TVlanID
+      fi
+    fi
+  else
+    #echo 'no conecta'
+    /usr/local/bin/ovs-vsctl show | grep tcp:$br0_ip:6633 > /dev/null
+    if [ $? -eq 1 ];then 
+      #Conecta a pox local
+      /usr/local/bin/ovs-vsctl set-controller br0 tcp:$br0_ip:6633
+    fi
+  fi
+  sleep 1
+done 
+
+
+#Parameters 
+# $1 controller_ip
+# $2 br0_ip
+# $3 TVlanID
