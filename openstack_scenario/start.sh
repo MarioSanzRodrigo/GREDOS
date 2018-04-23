abd0c7ee62e837539bee11edbe82527f28b4aa0e
Carlos Vega <c.vegag5@gmail.com>
2018-04-23 15:22:32 +0200

    Start installation OSM

--- openstack_scenario/start.sh
+++ openstack_scenario/start.sh
@@ -5,5 +5,5 @@ sleep 40
 vnx -f openstack_gredos.xml -x start-all,load-img
 sshpass -p 'xxxx' scp -o StrictHostKeyChecking=no conf/controller/glance/Xenial-GREDOS.qcow2 root@controller:/tmp/images
 vnx -f openstack_gredos.xml -x create-img-local
-vnx -f openstack_gredos.xml -x step9
-vnx -f openstack_gredos.xml -x create-tacker
+#vnx -f openstack_gredos.xml -x step9
+#vnx -f openstack_gredos.xml -x create-tacker
