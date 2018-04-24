5986cdf2abef0a2d4f91c79a9e6c805bb84f0a7f
Carlos Vega <c.vegag5@gmail.com>
2018-04-24 13:42:08 +0200

    OSM Configuration sequence

--- openstack_scenario/start.sh
+++ openstack_scenario/start.sh
@@ -3,7 +3,7 @@ vnx -f openstack_gredos.xml -v -t
 vnx_config_nat ExtNet eno1
 sleep 40
 vnx -f openstack_gredos.xml -x start-all,load-img
-sshpass -p 'xxxx' scp -o StrictHostKeyChecking=no conf/controller/glance/Xenial-GREDOS.qcow2 root@controller:/tmp/images/
-vnx -f openstack_gredos.xml -x create-img-local
+#sshpass -p 'xxxx' scp -o StrictHostKeyChecking=no conf/controller/glance/Xenial-GREDOS.qcow2 root@controller:/tmp/images/
+#vnx -f openstack_gredos.xml -x create-img-local
 #vnx -f openstack_gredos.xml -x step9
 #vnx -f openstack_gredos.xml -x create-tacker
